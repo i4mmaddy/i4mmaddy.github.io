4deaa1889ec64feb43682077eaf56e67d306ac4e
Madhavan Mani <maddyprsnl@gmail.com>
2021-07-09 21:11:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg',
+    }
+  }
+})
+
+
+app.mount('#app')
