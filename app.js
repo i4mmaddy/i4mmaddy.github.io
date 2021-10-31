3b9a434a674ae07b2df547d2dcabd2cb9a30b9df
Madhavan Mani <maddyprsnl@gmail.com>
2021-10-31 16:54:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
     return {
       firstName: 'John',
       lastName: 'Doe',
-      email: 'https://www.linkedin.com/in/m4dhavan/',
+      email: '',
       gender: 'male',
       picture: 'https://randomuser.me/api/portraits/men/10.jpg',
     }
